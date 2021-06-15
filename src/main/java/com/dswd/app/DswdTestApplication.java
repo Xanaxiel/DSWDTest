3f995698ffa567e63d5d3fb66b4fb3890e9f76ed
Cy Pangilinan <x.anaxiel-github@yahoo.com>
2021-06-15 13:19:44 +0800

    Initial commit

---
+++ src/main/java/com/dswd/app/DswdTestApplication.java
@@ -0,0 +1,13 @@
+package com.dswd.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswdTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DswdTestApplication.class, args);
+    }
+
+}
